eabcc6be8f83ae5014fbcdf19ec1989e8d236a74
PierreTSE <pierre--giraud@orange.fr>
2021-02-11 18:36:47 +0100

    README, minor improvements

--- scripts/upload_local_s3_ec2.py
+++ scripts/upload_local_s3_ec2.py
@@ -1,24 +1,25 @@
-import boto3
-import botocore
-import paramiko
-import os
 import configparser
+import os
+
+import boto3
+import paramiko
 
 os.path.dirname(os.path.abspath(__file__))
 config = configparser.RawConfigParser()
 config.read('properties.txt')
 
-KEY=config.get('AWS','aws_access_key_id')
-SECRET=config.get('AWS','aws_secret_access_key')
-Token=config.get('AWS','aws_session_token')
-bucket_name=config.get('AWS','bucket_name')
-dossier_local_data=config.get('AWS','dossier_local_data')
-dossier_local_result=config.get('AWS','dossier_local_result')
-username=config.get('AWS','username')
-hostname=config.get('AWS','hostname')
-paire_cle=config.get('AWS','paire_cle')
+KEY = config.get('AWS', 'aws_access_key_id')
+SECRET = config.get('AWS', 'aws_secret_access_key')
+Token = config.get('AWS', 'aws_session_token')
+bucket_name = config.get('AWS', 'bucket_name')
+dossier_local_data = config.get('AWS', 'dossier_local_data')
+dossier_local_result = config.get('AWS', 'dossier_local_result')
+username = config.get('AWS', 'username')
+hostname = config.get('AWS', 'hostname')
+paire_cle = config.get('AWS', 'paire_cle')
 
-s3 = boto3.client('s3', aws_access_key_id=KEY,aws_secret_access_key=SECRET,aws_session_token=Token, region_name='us-east-1')
+s3 = boto3.client('s3', aws_access_key_id=KEY, aws_secret_access_key=SECRET, aws_session_token=Token,
+                  region_name='us-east-1')
 
 
 # Methode pour créer un bucket
@@ -28,6 +29,7 @@ def create_bucket(bucket_name):
     except:
         return False
     return True
+
 
 # Methode pour transferer des fichiers en local à un bucket
 def upload_file(file_name, bucket, object_name=None):
@@ -40,29 +42,29 @@ def upload_file(file_name, bucket, object_name=None):
         return False
     return True
 
+
 # Methode pour transferer un dossier en local à un bucket
-def uploadDirectory(path,bucketname):
-        for root,dirs,files in os.walk(path):
-            for file in files:
-                s3.upload_file(os.path.join(root,file),bucketname, 'data/{}'.format(file))
+def uploadDirectory(path, bucketname):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            s3.upload_file(os.path.join(root, file), bucketname, 'data/{}'.format(file))
 
 
 create_bucket(bucket_name)
 # Restriction des acces
 
 response_public = s3.put_public_access_block(
-        Bucket=bucket_name,
-        PublicAccessBlockConfiguration={
-            'BlockPublicAcls': True,
-            'IgnorePublicAcls': True,
-            'BlockPublicPolicy': True,
-            'RestrictPublicBuckets': True
-        },
-    )
-    
+    Bucket=bucket_name,
+    PublicAccessBlockConfiguration={
+        'BlockPublicAcls': True,
+        'IgnorePublicAcls': True,
+        'BlockPublicPolicy': True,
+        'RestrictPublicBuckets': True
+    },
+)
 
 # Transfere du dossier en local au bucket
-uploadDirectory(dossier_local_data,bucket_name)
+uploadDirectory(dossier_local_data, bucket_name)
 
 # Connexion ssh a l'instance
 key = paramiko.RSAKey.from_private_key_file(paire_cle)
@@ -72,10 +74,9 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try:
     # Transfere des données du bucket à l'instance
     client.connect(hostname=hostname, username=username, pkey=key)
-    cmd="aws s3 cp s3://"+bucket_name+"/data /home/ec2-user/data --recursive"
+    cmd = "aws s3 cp s3://" + bucket_name + "/data /home/ec2-user/data --recursive"
     stdin, stdout, stderr = client.exec_command(cmd)
     print(stdout.read())
     client.close()
 except Exception:
     print("ERREUR CONNEXION")
-
